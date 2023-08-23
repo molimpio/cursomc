93c4cce376e8227a0da368c4cdbe54b53d9b258f
Moisés Olimpio <molimpio@hotmail.com.br>
2023-08-23 09:02:22 -0300

    Criando classe de domínio categoria, testando endpoint

--- src/main/java/com/molimpio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/molimpio/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,24 @@
 package com.molimpio.cursomc.resources;
 
+import com.molimpio.cursomc.domain.Categoria;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String listarCategorias() {
-        return "REST está funcionando OK";
+    public List<Categoria> listarCategorias() {
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+        List<Categoria> categorias = new ArrayList<>();
+        categorias.add(cat1);
+        categorias.add(cat2);
+        return categorias;
     }
 }
