0bd675bdc703b3e3712c6ab12d8e6389260380c6
Moisés Olimpio <molimpio@hotmail.com.br>
2023-08-23 09:54:34 -0300

    Criando Repository, Service para Categoria
    Criando repository e service para entidade categoria, alterou o endpoint para retornar uma categoria ou nulo caso não encontre

--- src/main/java/com/molimpio/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/molimpio/cursomc/resources/CategoriaResource.java
@@ -1,24 +1,30 @@
 package com.molimpio.cursomc.resources;
 
 import com.molimpio.cursomc.domain.Categoria;
+import com.molimpio.cursomc.services.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
-    @RequestMapping(method = RequestMethod.GET)
-    public List<Categoria> listarCategorias() {
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
-        List<Categoria> categorias = new ArrayList<>();
-        categorias.add(cat1);
-        categorias.add(cat2);
-        return categorias;
+    @Autowired
+    private CategoriaService service;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<?> find(@PathVariable Integer id) {
+        Categoria obj = service.buscar(id);
+        return ResponseEntity.ok().body(obj);
     }
+
+
 }
